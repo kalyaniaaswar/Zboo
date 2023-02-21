adcb93f2c14a311830632d0fd96babcf2eeef687
Rushabh <Rushabh@DESKTOP-BJTS1NM>
2023-02-21 12:55:11 +0530

    first comot

---
+++ src/zboo1/zbo.java
@@ -0,0 +1,15 @@
+package zboo1;
+
+public class zbo {
+
+	public static void main(String[] args) {
+		int b=1;
+		int fact=1;
+		for(int a=1; a<=5; a++) {
+			fact=fact*a;
+			
+		}
+	   System.out.println(fact);
+	}
+
+}
